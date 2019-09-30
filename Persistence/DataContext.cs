1c60e42c7f013acc624cf34e1b481b2fd06afcbd
arpethel <arpethel@gmail.com>
2019-09-30 10:04:09 -0400

    applying dependency injection

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,5 +11,15 @@ namespace Persistence
         }
 
         public DbSet<Value> Values {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+                new Value {Id = 1, name = "Value 101"},
+                new Value { Id = 2, name = "Value 102" },
+                new Value { Id = 3, name = "Value 103" }
+            );
+        }
     }
 }
