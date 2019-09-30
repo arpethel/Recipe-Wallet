12b89edf7704a8d5aadcce744b652c86ea49d00b
arpethel <arpethel@gmail.com>
2019-09-30 13:28:49 -0400

    installed and employed axios

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   state = {
@@ -8,9 +9,13 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      values: [{id: 1, name: 'Value 101'}, {id: 2, name: 'Value 102'}]
-    })
+    axios.get('http://localhost:5000/api/values')
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          values: response.data
+        })
+      })
   }
 
   render() {
