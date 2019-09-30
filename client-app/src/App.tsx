8f5b9858a2c4ec5f109ca16248babb5891f434be
arpethel <arpethel@gmail.com>
2019-09-30 14:02:00 -0400

    added semantic ui

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import { Header, Icon, List } from 'semantic-ui-react'
 import './App.css';
 import axios from 'axios';
 
@@ -11,7 +11,6 @@ class App extends Component {
   componentDidMount() {
     axios.get('http://localhost:5000/api/values')
       .then((response) => {
-        console.log(response);
         this.setState({
           values: response.data
         })
@@ -20,15 +19,16 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul>
-            {this.state.values.map((value: any) => (
-              <li key={value.id}>{value.name}</li>
-            ))}
-          </ul>
-        </header>
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Recipe Wallet</Header.Content>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
       </div>
     );
   }
